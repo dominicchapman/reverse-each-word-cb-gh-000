2c1f6f3cfa91a5bf75b407258350144d25d25b6a
Dominic Chapman <dominicchapman@me.com>
2017-10-04 20:54:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
 
 def reverse_each_word(phrase)
-  array = phrase.split(' ')
-  array.each do |word|
-    word.reverse!
-  end
-  # .each would return original
-  return array.join(' ')
+  phrase.split.each {|word| word.reverse}.join(' ')
 end
 
 # def reverse_each_word(phrase)
