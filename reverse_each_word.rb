e2b9fd9fe62fe5d1e52ca370ff1c87da59dd99cd
Dominic Chapman <dominicchapman@me.com>
2017-10-04 22:22:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 
 def reverse_each_word(phrase)
-  new = phrase.split.each {|word| word.reverse}.join(' ')
-  return new
+  # without bang operator (!), we'd get phrase.split
+  # since each returns the object it was called on
+  phrase.split.each { |word| word.reverse! }.join(' ')
 end
 
-# def reverse_each_word(phrase)
-#   array = phrase.split(' ')
-#   array.collect { |word| word.reverse! }.join(' ')
-# end
+
+def reverse_each_word(phrase)
+  phrase.split.collect { |word| word.reverse }.join(' ')
+end
